f1bfc91367f3b8604c06601560b48f837606bd68
aupreti1 <anaupreti@gmail.com>
2019-07-16 00:18:52 -0400

    added the footer

---
+++ client/src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+	<footer className = "page-footer">
+		<div className = "container-fluid text-center">
+			<a href="https://github.com/aupreti1">GitHub</a>
+		</div>	
+	</footer>
+);
+
+export default Footer;
